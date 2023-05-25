6eab17896ebea54bfce28497398b80d5e5c65d9e
kayjeee <kagiso.killagram@gmail.com>
2023-05-25 17:59:46 +0200

    added API calls

--- src/index.js
+++ src/index.js
@@ -2,28 +2,37 @@ import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 
-// Your remaining JavaScript code goes here
+// Your remaining JavaScript code goes her
 
 // Import modules
-import { fetchData, renderList } from '../modules/utils.js';
+import { fetchmyScore, addmyScore } from '../modules/utils.js';
 // Import modules
 
-// Get DOM elements
-const btn = document.getElementById('btn');
-const list = document.getElementById('list');
-const form = document.getElementById('form');
+// Get DOM element
+const form = document.querySelector('form');
+const name = document.querySelector('.name');
+const score = document.querySelector('.score');
 
-// Button click event listener
-// Button click event listener
-btn.addEventListener('click', async () => {
-  // Fetch data
-  const data = await fetchData();
-
-  // Render list
-  renderList(data, list);
+// event for refersh button
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+  window.location.reload();
 });
 
+fetchmyScore();
 // Form submit event listener
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const nameValue = name.value.trim();
+  const scoreValue = score.value.trim();
+
+  if (nameValue !== '' && scoreValue !== '') {
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData);
+    addmyScore(data);
+
+    name.value = '';
+    score.value = '';
+  }
 });
